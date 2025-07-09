2cd9402b013c11becd69863785c98183e5d76be9
sur <surh@users.noreply.github.com>
2025-07-09 15:14:44 -0600

    Calculating Rhat

--- fit_growth/functions.r
+++ fit_growth/functions.r
@@ -1,3 +1,7 @@
+box::use(magrittr[...])
+
+
+#' @export
 calculate_stats <- function(Dat, column = "r"){
 
     par.mcmc <- coda::as.mcmc(Dat[[column]])
@@ -8,17 +12,29 @@ calculate_stats <- function(Dat, column = "r"){
     hpd10 <- hpd[1]
     hpd90 <- hpd[2]
 
+
+    Rhat <- Dat %>%
+        dplyr::select(!!column, chain, iteration) %>%
+        tidyr::pivot_wider(names_from = chain, values_from = !!column) %>%
+        dplyr::select(-iteration) %>%
+        as.matrix() %>%
+        rstan::Rhat()
+
     dplyr::tibble(parameter = column,
            mean = mean(as.numeric(Dat[[column]]), na.rm = TRUE),
            sd = stats::sd(as.numeric(Dat[[column]]), na.rm = TRUE),
            median = stats::median(as.numeric(Dat[[column]]), na.rm = TRUE),
-           q05 = stats::quantile(as.numeric(Dat[[column]]), 0.05, na.rm = TRUE),
-           q10 = stats::quantile(as.numeric(Dat[[column]]), 0.1, na.rm = TRUE),
-           q90 = stats::quantile(as.numeric(Dat[[column]]), 0.9, na.rm = TRUE),
-           q95 = stats::quantile(as.numeric(Dat[[column]]), 0.95, na.rm = TRUE),
            hpd05 = hpd05,
            hpd10 = hpd10,
            hpd90 = hpd90,
-           hpd95 = hpd95
+           hpd95 = hpd95,
+           Rhat = Rhat,
+           q05 = stats::quantile(as.numeric(Dat[[column]]), 0.05, na.rm = TRUE),
+           q10 = stats::quantile(as.numeric(Dat[[column]]), 0.1, na.rm = TRUE),
+           q90 = stats::quantile(as.numeric(Dat[[column]]), 0.9, na.rm = TRUE),
+           q95 = stats::quantile(as.numeric(Dat[[column]]), 0.95, na.rm = TRUE)
+
     )
 }
+
+
--- fit_growth/process_single_strain_bytemp_logistic.r
+++ fit_growth/process_single_strain_bytemp_logistic.r
@@ -3,7 +3,6 @@ library(coda)
 
 box::use(./fit_growth/functions)
 
-
 args <- list()
 args$indir <- "/Users/sur/lab/exp/2025/today3/single_strain_bytemp_logistic/"
 infiles <- list.files(args$indir, pattern = "_logistic_fit.tsv", full.names = TRUE)
@@ -29,15 +28,6 @@ for(f in infiles){
 }
 Res
 
-Dat
-Dat %>%
-    select(K, chain, iteration) %>%
-    pivot_wider(names_from = chain, values_from = K) %>%
-    select(-iteration) %>%
-    as.matrix() %>%
-    rstan::Rhat()
-
-
 
 p1 <- Res %>%
     filter(parameter == "r") %>%
@@ -54,12 +44,12 @@ p1
 
 p1 <- Res %>%
     filter(parameter == "K") %>%
-    ggplot(aes(x = mean, y = temp)) +
-    facet_wrap(~strain, scales = "fixed") +
-    geom_linerange(aes(xmin = hpd05, xmax = hpd95), size = 1, color = "darkgrey") +
-    geom_linerange(aes(xmin = hpd10, xmax = hpd90), size = 3, color = "darkgrey") +
+    ggplot(aes(x = temp, y = mean)) +
+    facet_grid(. ~ strain, scales = "free_y") +
+    geom_linerange(aes(ymin = hpd05, ymax = hpd95), linewidth = 1, color = "darkgrey") +
+    geom_linerange(aes(ymin = hpd10, ymax = hpd90), linewidth = 3, color = "darkgrey") +
     geom_point(size = 6, color = "#dd1c77") +
-    geom_point(aes(x = median), size = 5, color = "#c994c7") +
-    labs(x = "Carrying capacity (OD600)", y = "Strain") +
+    geom_point(aes(y = median), size = 5, color = "#c994c7") +
+    labs(y = "Growth rate (1/h)", x = "Temperature (ºC)") +
     theme_classic()
 p1
