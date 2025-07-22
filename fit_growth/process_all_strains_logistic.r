358feb70c2a61aa7e695623f2ad1840ef0e3ac9a
sur <surh@users.noreply.github.com>
2025-07-22 12:48:49 -0600

    cleanup

--- fit_growth/process_all_strains_logistic.r
+++ fit_growth/process_all_strains_logistic.r
@@ -164,7 +164,7 @@ p1 <- Res %>%
     geom_linerange(aes(ymin = hpd10, ymax = hpd90), linewidth = 3, color = "darkgrey") +
     geom_point(size = 6, color = "#dd1c77") +
     geom_point(aes(y = median), size = 5, color = "#c994c7") +
-    labs(y = "Growth rate (1/h)", x = "Temperature (ºC)") +
+    labs(y = "Carrying capacity (K)", x = "Temperature (ºC)") +
     theme_classic()
 p1
 outfile <- file.path(args$outdir, "all_strains_logistic_K.png")
